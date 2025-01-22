844b2e444b54d9734189951a272a04d7c18e82a5
julesroy <112734731+julesroy@users.noreply.github.com>
2025-01-22 22:34:07 +0100

    Separated standings in 2 routes

--- routes/standings.js
+++ routes/standings.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
-// fonctions
-const getDriversActualStandings = require("../utils/standings/getDriversActualStanding"); // Fonction permettant de récupérer le classement actuel des pilotes
-const getTeamsActualStandings = require("../utils/standings/getConstructorsActualStandings"); // Fonction permettant de récupérer le classement actuel des écuries
+// functions
+const getDriversActualStandings = require("../utils/standings/getDriversActualStanding");
+const getTeamsActualStandings = require("../utils/standings/getConstructorsActualStandings");
 
-router.get("/standings", async (req, res) => {
+router.get("/driverstandings", async (req, res) => {
     try {
-        var actualDriversStanding = await getDriversActualStandings(); // Récupération du classement actuel des pilotes
-        var actualTeamsStanding = await getTeamsActualStandings(); // Récupération du classement actuel des écuries
-        res.render("standings/standings", { actualDriversStanding: actualDriversStanding, actualTeamsStanding: actualTeamsStanding });
+        var actualDriversStanding = await getDriversActualStandings();
+        res.render("standings/driversStandings", { actualDriversStanding: actualDriversStanding });
     } catch (error) {
-        res.render('security/error', { textError: '/standings route, error during execution', error: error });
+        res.render('security/error', { textError: '/driverstandings route, error during execution', error: error });
+    }
+});
+
+router.get("/constructorstandings", async (req, res) => {
+    try {
+        var actualTeamsStanding = await getTeamsActualStandings();
+        res.render("standings/constructorsStandings", { actualTeamsStanding: actualTeamsStanding });
+    } catch (error) {
+        res.render('security/error', { textError: '/constructorstandings route, error during execution', error: error });
     }
 });
 
