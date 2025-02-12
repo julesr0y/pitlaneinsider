c16861de158e2db1e2871772cc4e7dabd511cfe2
julesroy <112734731+julesroy@users.noreply.github.com>
2025-02-12 12:08:29 +0100

    Test new ui for former drivers page

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
     'border-scarab', 'border-schroeder', 'border-scirocco', 'border-shadow', 'border-shannon', 'border-sherman', 'border-simca-gordini', 'border-simtek', 'border-snowberger', 'border-spirit', 'border-spyker', 'border-stebro', 'border-stevens', 'border-stewart', 'border-super-aguri', 'border-surtees', 'border-sutton', 'border-talbot-lago', 'border-tec-mec', 'border-tecno', 'border-theodore', 'border-token', 'border-toleman', 'border-toro-rosso', 'border-toyota', 'border-trevis', 'border-trojan', 'border-turner', 'border-tyrrell', 'border-vanwall', 'border-venturi', 'border-veritas', 'border-virgin', 'border-watson', 'border-wetteroth', 'border-wolf-williams', 'border-wolf', 'border-zakspeed',
 
     // user theme
-    'main-color', 'bg-light', 'bg-b-light', 'bg-s-light', 'text-t-s-light', 'text-light', 'border-light', 'bg-dark', 'bg-b-dark', 'bg-s-dark', 'text-t-s-dark', 'text-dark', 'text-hasnt-finished-race', 'border-dark'
+    'main-color', 'bg-light', 'bg-b-light', 'bg-s-light', 'text-t-s-light', 'text-light', 'border-light', 'bg-dark', 'bg-b-dark', 'bg-s-dark', 'bg-t-dark', 'text-t-s-dark', 'text-dark', 'text-hasnt-finished-race', 'border-dark'
   ],
   theme: {
     extend: {
@@ -32,6 +32,7 @@ module.exports = {
         'light': 'black',
         'b-dark': '#191919',
         's-dark': '#333333',
+        't-dark': '#058785',
         't-s-dark': '#d1d5db',
         'dark': 'white',
         'deconnect': '#F6A60B',
